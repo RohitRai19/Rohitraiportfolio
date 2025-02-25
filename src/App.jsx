4c3a47f2ba1e8a2292e6ccdd4485865957f675a2
Rohit Rai <rairohit385@gmail.com>
2025-02-25 16:31:21 +0530

    Navbar - create navbar and all create all pages to design on them

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Navbar from "./component/Navbar";
+import About from "./component/About";
+import Service from "./component/Service/Service";
+import Skill from './component/Skills'
+import Project from "./component/Projects";
+import Testimonials from "./component/Testimonials";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Subscribe for more videos</h1>
+              <Navbar/>
+            <About/>
+             <Service/>
+             <Skill/>
+             <Project/>
+             <Testimonials/>
+             <Contact/>
+             <Footer/>
+
     </>
   );
 };
