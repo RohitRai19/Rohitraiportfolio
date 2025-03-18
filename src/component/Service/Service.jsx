e4569f3065cf97a3e0072dca880424b098c2cb92
Rohit Rai <rairohit385@gmail.com>
2025-03-18 17:36:46 +0530

    Add service section with responisveness

--- src/component/Service/Service.jsx
+++ src/component/Service/Service.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './Service.css'
+import React from "react";
+import "./Service.css";
+import { services } from "../../data";
+import ServiceCard from "../../ui/ServiceCard";
 function Service() {
   return (
-    <div>Service</div>
-  )
+    <section id="service">
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">Service</h2>
+        </div>
+        <div className="service__group">
+          {services.map(
+            ({ title, description, icon, color, colorRGB }, index) => (
+              <ServiceCard 
+              title={title}
+              description={description}
+              icon={icon}
+              color={color}
+              colorRGB={colorRGB}
+              key={index} />
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Service
+export default Service;
