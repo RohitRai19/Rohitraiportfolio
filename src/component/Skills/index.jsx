4e3cf5bbb373006b6a36a38a41526c24fe164ce6
Rohit Rai <rairohit385@gmail.com>
2025-03-19 11:03:01 +0530

    improve skill section with img and cards

--- src/component/Skills/index.jsx
+++ src/component/Skills/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Skill.css'
+import { skills } from '../../data'
+import SkillCard from '../../ui/SkillCard'
 function Skill() {
   return (
-    <div>Skill</div>
+    <section id='skills'>
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">My Skills</h2>
+        </div>
+
+        <div className="skill__container">{skills.map((list,index)=>(<SkillCard {...list} key={index}/>))}</div>
+      </div>
+    </section>
   )
 }
 
